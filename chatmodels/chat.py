d83d622d8097a1737cdbf5a1bdf98380d6044a30
BADAL SINGH <singhbaadal619@gmail.com>
2026-08-21 22:33:36 +0530

    Updated GenAI learning files

---
+++ chatmodels/chat.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain.chat_models import init_chat_model
+
+model = init_chat_model("groq:openai/gpt-oss-120b")
+response=model.invoke("tell me something about machine learning in one paragraph")
+print(response.content)
---
+++ embeddingmodels/huggingfacae_embeddding.py
@@ -0,0 +1,20 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+embedding = HuggingFaceEmbeddings(
+    model_name = "sentence-transformers/all-MiniLM-L6-v2"
+
+)
+texts = [
+    "Hello this is Akarsh Vyas",
+    "Hello your name is YouTube",
+    "And you all are very beautiful"
+]
+
+
+
+vector = embedding.embed_documents(texts)
+
+print(vector)
